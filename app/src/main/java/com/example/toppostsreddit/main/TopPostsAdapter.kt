7c485e7e4bbd78e45de99e83f9b47de1bc2e6f10
Zhamtsyn <renatazh456@gmail.com>
2022-12-25 20:45:49 +0200

    add the "hours ago" information to posts

--- app/src/main/java/com/example/toppostsreddit/main/TopPostsAdapter.kt
+++ app/src/main/java/com/example/toppostsreddit/main/TopPostsAdapter.kt
@@ -9,6 +9,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.toppostsreddit.data.models.Children
 import com.example.toppostsreddit.databinding.ItemPostBinding
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.roundToInt
 
 class TopPostsAdapter(
     private val onClickListener: (String) -> Unit,
@@ -17,10 +20,12 @@ class TopPostsAdapter(
     inner class TopPostsViewHolder(private val binding: ItemPostBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(post: Children) {
+            val time = getHours(post.data.created_utc.toLong(), "HH")
             with(binding) {
                 tvAuthor.text = post.data.author
                 tvComments.text = "Comments:${post.data.num_comments}"
                 tvText.text = post.data.title
+                tvHoursAgo.text = "$time hours ago"
                 if (post.data.thumbnail == "default") {
                     iv.isVisible = false
                     tvSave.isVisible = false
@@ -54,8 +59,21 @@ class TopPostsAdapter(
         }
     }
 
-    override fun getItemCount(): Int {
-        return dataList.size
+    fun getHours(secondsToAdd: Long, pattern: String): String {
+        val sdf = SimpleDateFormat(pattern, Locale.getDefault())
+        val netDate = Date(secondsToAdd * 1000)
+        val time = sdf.format(netDate)
+        val minuteTime = SimpleDateFormat("mm").format(netDate)
+
+        val c = Calendar.getInstance()
+
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+
+        val res1 = ((time.toInt() * 60 + minuteTime.toInt()) / 60.0).roundToInt()
+        val res2 = ((hour * 60 + minute) / 60.0).roundToInt()
+
+        return if (time.toInt() >= hour) (24 - res1 + res2).toString() else (res2 - res1).toString()
     }
 
     companion object {
